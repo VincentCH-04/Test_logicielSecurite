0a65c06e692c18b2bc60c335090ca115f7dea42e
VincentCH-04 <154899418+VincentCH-04@users.noreply.github.com>
2024-12-04 14:53:36 +0100

    FooterBar.vue

---
+++ src/components/FooterBar.vue
@@ -0,0 +1,85 @@
+<template>
+    <footer class="footer">
+        <div class="footer-container">
+        <div class="footer-section">
+            <a @click="toggleContact">Contact</a>
+            <p v-if="contactVisu" class="footer-section">
+                vincent.chaumette@etu.univ-tours.fr
+            <br>ewen.lapaix@etu.univ-tours.fr
+            <br>flavien.absolu@etu.univ-tours.fr
+            <br>mehdi.sibous@etu.univ-tours.fr
+            <br>timothee.debey@etu.univ-tours.fr
+            <br>dimitri.britan@etu.univ-tours.fr
+            </p>
+        </div>
+        <div class="footer-section">
+            © Tous droits réservés 2024 Locamat
+        </div>
+        <div class="footer-section">
+            Version 1.0
+        </div>
+        <div @click="toggleAPropos" class="footer-section">
+            À propos de Locamat
+            <div v-if="AProposVisu" class="footer-columns">
+                <div class="footer-column">
+                    <p>Vincent CHAUMETTE</p>
+                    <p>Ewen LAPAIX</p>
+                    <p>Flavien ABSOLU</p>
+                </div>
+                <div class="footer-column">
+                    <p>Mehdi SIBOUS</p>
+                    <p>Timothee DEBEY</p>
+                    <p>Dimitri BRITAN</p>
+                </div>
+            </div>
+        </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'FooterBar',
+    data() {
+        return{
+            contactVisu: false,
+            AProposVisu: false
+        };
+    },
+    methods: {
+        toggleContact() {
+            this.contactVisu = !this.contactVisu;
+        },
+        toggleAPropos() {
+            this.AProposVisu = !this.AProposVisu;
+        }
+    }
+};
+</script>
+
+<style scoped>
+.footer {
+  width: 100%;
+  background-color: #000000;
+  padding: 10px 0;
+  position: absolute;
+  bottom: 0;
+}
+
+.footer-container {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-wrap: wrap;
+}
+
+.footer-section {
+    margin: 5px;
+    color: white;
+}
+
+.footer-columns {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
